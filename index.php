6b7deaa6578c3fca739a7f323a2eec88a60b2920
much yusron arif <yusron.arif4@gmail.com>
2016-05-10 15:14:18 +0700

    move https redirect to server config (.htaccess via apache)

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
     $redirect = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
     header("HTTP/1.1 301 Moved Permanently");
     header("Location: $redirect");
-}
-date_default_timezone_set('Asia/Jakarta');
+}*/
+//date_default_timezone_set('Asia/Jakarta');
 /**
  * CodeIgniter
  *
